d1682c2b7af619d5f73e34f62191a4df9ff24639
snehabichkunde <bichkundesneha@gmail.com>
2025-04-07 12:52:49 +0530

    Hardcoded Dashboard and SendMoney pages; backend integration pending

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
-export function Dashboard(){
-    return (
-        <>
-            <p>Dashboard</p>
-        </>
-    )
+import { Appbar} from "../components/Appbar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
+
+
+export const Dashboard = () => {
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"10,000"}/>
+            <Users />
+        </div>
+    </div>
 }
